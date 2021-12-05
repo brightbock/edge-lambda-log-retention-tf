4acdba528f2b99d9f818716a0f07ec7d73b5a17e
JamesJJ <jj@fcg.fyi>
2021-12-06 00:03:35 +0800

    terraform fmt

--- each_region/log_group.tf
+++ each_region/log_group.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "log_group" {
-  count = (var.destroy ? 0 : 1)
+  count             = (var.destroy ? 0 : 1)
   provider          = aws.each_region
   name              = var.region_log_group_name
   retention_in_days = var.retention_in_days
